95126ebd0052d415e8e04753f436d4748da71cb1
ennegrox23 <franmegapro10@gmail.com>
2025-03-19 22:40:04 -0300

    entregado todos los puntos del 2do diagnostico

--- 19-3/Program.cs
+++ 19-3/Program.cs
@@ -112,11 +112,89 @@ namespace _12_3
                         {
                             Console.WriteLine("Error: Seleccione un numero valido");
                         }
-                    } while (true);
+                    } while (N == 1 || N == 2 || N == 3 || N == 4 || N == 5 || N == 6 || N == 7);
                     break;
                 case 8:
+                    do
+                    {
+                        Console.WriteLine("Ingrese Un Numero Del 1 Al 7");
+                        N = int.Parse(Console.ReadLine());
+                        if (N == 1)
+                        {
+                            Console.WriteLine("enero");
+                        }
+                        else if (N == 2)
+                        {
+                            Console.WriteLine("febrero");
+                        }
+                        else if (N == 3)
+                        {
+                            Console.WriteLine("marzo");
+                        }
+                        else if (N == 4)
+                        {
+                            Console.WriteLine("abril");
+                        }
+                        else if (N == 5)
+                        {
+                            Console.WriteLine("mayo");
+                        }
+                        else if (N == 6)
+                        {
+                            Console.WriteLine("junio");
+                        }
+                        else if (N == 7)
+                        {
+                            Console.WriteLine("julio");
+                        }
+                        else if (N == 8)
+                        {
+                            Console.WriteLine("agosto");
+                        }
+                        else if (N == 9)
+                        {
+                            Console.WriteLine("septiembre");
+                        }
+                        else if (N == 10)
+                        {
+                            Console.WriteLine("octubre");
+                        }
+                        else if (N == 11)
+                        {
+                            Console.WriteLine("noviembre");
+                        }
+                        else if (N == 12)
+                        {
+                            Console.WriteLine("Dicienmbre");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Seleccione un numero valido");
+                        }
+                    } while (N == 1 || N == 2 || N == 3 || N == 4 || N == 5 || N == 6 || N == 7 || N == 8 || N == 9 || N == 10 || N == 11 || N == 12);
                     break;
                 case 9:
+                    string respuesta;
+                    do
+                    {
+                        int resultado;
+                        Console.WriteLine("Ingrese Un Numero");
+                        N = int.Parse(Console.ReadLine());
+                        resultado = N % 2;
+                        if (resultado == 1)
+                        {
+                            Console.WriteLine("Impar");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Par");
+                        }
+                        Console.WriteLine("Desea Consultar Otro Numero? Y/N");
+                       
+                        respuesta = Console.ReadLine();
+                    } while (respuesta == "s" || respuesta == "S");
+                    break;
+                default:
                     break;
             }
             Console.ReadKey();
